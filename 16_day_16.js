e00cf2fb469c54a91ff00a60d490a7260c8f5258
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-02-17 18:24:11 +0530

    Improvement of the code in day 16 of the challenge

--- 16_day_16.js
+++ 16_day_16.js
@@ -5,7 +5,9 @@ function connectToMongoDB() {
   const uri = process.env.MONGODB_URI;
   mongoose.connect(uri);
   const db = mongoose.connection;
-  db.on("error", console.error.bind(console, "connection error:"));
+  db.on("error", function (err) {
+    console.log("Error connecting to MongoDB Atlas!", err);
+  });
   db.once("open", function () {
     console.log("Connected to MongoDB Atlas!, Successfully!");
   });
